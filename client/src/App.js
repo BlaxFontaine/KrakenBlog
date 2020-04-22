defaac197bb84daebc0fb62f4f6560cdd41272e0
Blax Fontaine <valerie.gehle@epitech.eu>
2020-04-22 12:22:16 +0200

    user registration form in front and route in back

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import UserSignUp from './components/UserSignUp';
 
 function App() {
   return (
-    <div className="App">
-      Hello world!
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/register" component={UserSignUp} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
