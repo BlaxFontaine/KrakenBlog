e14c806180f5aa8f7443cc05c8dd6d5a87ae9e18
Blax Fontaine <valerie.gehle@epitech.eu>
2020-04-22 10:04:12 +0200

    base app

---
+++ server/server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var app = express();
+
+// mongodb connection
+mongoose.connect("mongodb://localhost:27017/krakenblog", { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+
+//mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// include routes
+// var routes = require('./routes/index');
+// app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+// listen on port 5000
+app.listen(5000, function () {
+  console.log('Express app listening on port 5000');
+});
