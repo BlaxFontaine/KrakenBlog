6ec073b11d2d40463d6baf6e818e2de603ac4d89
nemesis <nemesis.srour@epitech.eu>
2020-04-23 17:11:28 +0200

    Update Fixed Bug Log In

--- server/routes/index.js
+++ server/routes/index.js
@@ -23,7 +23,7 @@ router.get('/logout', function(req, res, next) {
       if(err) {
         return next(err);
       } else {
-        return res.redirect('/');
+        console.log(err);
       }
     });
   }
@@ -34,30 +34,33 @@ router.get('/login', mid.loggedOut, function(req, res, next) {
   return res.render('login', { title: 'Log In'});
 });
 
+
 // POST /login
 router.post('/login', function(req, res, next) {
-  if (req.body.username && req.body.password) {
-    User.authenticate(req.body.username, req.body.password, function (error, user) {
+  console.log(req.body);
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function (error, user) {
       if (error || !user) {
-        var err = new Error('Wrong username or password.');
+        var err = new Error('Wrong email or password.');
         err.status = 401;
         return next(err);
       }  else {
         req.session.userId = user._id;
-        return res.redirect('/profile');
+        console.log('You are logged in.')
+        
       }
-    });
+    })
   } else {
-    var err = new Error('Username and password are required.');
+    var err = new Error('Email and password are required.');
     err.status = 401;
     return next(err);
   }
 });
 
-// GET /register
+/*// GET /register
 router.get('/register', mid.loggedOut, function(req, res, next) {
   return res.render('register', { title: 'Sign Up' });
-});
+});*/
 
 // POST /register
 router.post('/register', function(req, res, next) {
