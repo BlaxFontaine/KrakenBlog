18a56d910f480c8ae106a084cd4589d4bf08da24
nemesis <nemesis.srour@epitech.eu>
2020-04-23 19:15:05 +0200

    User Profile

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
-import UserSignUp from './components/UserSignUp';
-import UserLogIn from './components/UserLogIn';
+import UserSignUp from './components/Auth/UserSignUp';
+import UserLogIn from './components/Auth/UserLogIn';
+import UserProfile from './components/UserProfile';
+import MembersList from './components/MembersList';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/register" component={UserSignUp} />
           <Route path="/login" component={UserLogIn} />
+          <Route path="/profile" component={UserProfile} />
+          <Route path="/list" component={MembersList} />
         </Switch>
       </BrowserRouter>
     </div>
